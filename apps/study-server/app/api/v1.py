bed1190f11a1a92ce03ab8a805a2de03a69fcbe8
Đào Văn Hùng <daohungmt2005@gmail.com>
2026-07-25 22:49:53 +0700

    database: add data

--- apps/study-server/app/api/v1.py
+++ apps/study-server/app/api/v1.py
@@ -1,4 +1,7 @@
+from sqlalchemy import text 
+
 from fastapi import APIRouter
+from app.core.database import engine
 
 router = APIRouter(
     prefix="/api/v1",
@@ -9,3 +12,9 @@ router = APIRouter(
 @router.get("/hello")
 def hello_world():
     return {"message": "hello world!"}
+
+@router.get("/test/db")
+def test_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT NOW()"))
+        return {"result": [row[0] for row in result]}
